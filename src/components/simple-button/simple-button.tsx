775992a5a8f55bf0d97e4b07a219ec381e5c7da8
michaelauderer <mike@silverstone.io>
2018-09-30 03:25:31 -0400

    fix ripple

--- src/components/simple-button/simple-button.tsx
+++ src/components/simple-button/simple-button.tsx
@@ -14,48 +14,38 @@ export class ButtonComponent {
 
   @Element() buttonEl: HTMLElement
 
-  x: number
-  y: number
-  w: number
-  h: number
-
-  componentDidLoad() {
-    this.x = this.buttonEl.offsetLeft
-    this.y = this.buttonEl.offsetTop
-    this.w = this.buttonEl.offsetWidth
-    this.h = this.buttonEl.offsetHeight
+  handleClick = (event) => {
+    const buttonX = this.buttonEl.offsetLeft
+    const buttonY = this.buttonEl.offsetTop
+    let width = this.buttonEl.offsetWidth
+    let height = this.buttonEl.offsetHeight
 
     // make the ripple round
-    if (this.w > this.h) {
-      this.h = this.w
+    if (width > height) {
+      height = width
     } else {
-      this.w = this.h
+      width = height
     }
-  }
 
-  handleClick = (event) => {
-    console.log(event)
-    const x = event.pageX - this.x - this.w / 2
-    const y = event.pageY - this.y - this.h / 2
+    const x = event.pageX - buttonX - width / 2
+    const y = event.pageY - buttonY - height / 2
 
     const rippleStyles = {
-      width: this.w + 'px',
-      height: this.h + 'px',
+      width: width + 'px',
+      height: height + 'px',
       top: y + 'px',
       left: x + 'px'
     }
-
-    console.log(rippleStyles)
 
     this.ripples = [...this.ripples, (<span class="ripple" style={rippleStyles} />)]
   }
 
   render() {
-    return [
+    return (
       <button class={`${this.type} ${this.color}`} onClick={this.handleClick}>
         {...this.ripples}
         <slot />
       </button>
-    ]
+    )
   }
 }
